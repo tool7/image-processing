2f5c032c03fda15f1fc068106a431bfb0ddd7dd9
Marko Roje <marko.roje@gmail.com>
2022-12-24 12:15:17 +0100

    Implement custom app navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,9 @@
 <script lang="ts" setup>
-import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-  <img id="logo" alt="Wails logo" src="./assets/images/logo-universal.png" />
-  <HelloWorld />
+  <Navbar />
 </template>
 
-<style>
-#logo {
-  display: block;
-  width: 50%;
-  height: 50%;
-  margin: auto;
-  padding: 10% 0 0;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-  background-origin: content-box;
-}
-</style>
+<style></style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,76 @@
+<script lang="ts" setup>
+import { WindowMinimise, WindowToggleMaximise, Quit, WindowIsMaximised } from "../../wailsjs/runtime/runtime";
+
+const onMinimise = () => WindowMinimise();
+const onToggleMaximise = () => WindowToggleMaximise();
+const onQuit = () => Quit();
+</script>
+
+<template>
+  <div id="navbar" style="--wails-draggable: drag">
+    <span id="title">Go Image Processing</span>
+
+    <div>
+      <v-btn
+        class="btn"
+        :width="32"
+        :height="32"
+        variant="text"
+        icon="fas fa-minus"
+        :rounded="0"
+        style="--wails-draggable: none"
+        @click="onMinimise"
+      />
+      <v-btn
+        class="btn"
+        :width="32"
+        :height="32"
+        variant="text"
+        icon="fas fa-stop"
+        :rounded="0"
+        style="--wails-draggable: none"
+        @click="onToggleMaximise"
+      />
+      <v-btn
+        id="btn-quit"
+        class="btn"
+        :width="32"
+        :height="32"
+        variant="text"
+        icon="fas fa-sharp fa-xmark"
+        :rounded="0"
+        style="--wails-draggable: none"
+        @click="onQuit"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 32px;
+  border-bottom: 1px solid rgba(40, 40, 40, 1);
+  font-size: 12px;
+  margin: auto;
+}
+
+#title {
+  padding: 10px;
+  text-align: center;
+  color: var(--color-white);
+  cursor: default;
+  user-select: none;
+}
+
+#btn-quit:hover {
+  background-color: red;
+}
+
+.btn {
+  font-size: 10px !important;
+}
+</style>
