2aee303c5e24a101bd25884c09faa635880ee88c
Marko Roje <marko.roje@gmail.com>
2022-12-28 15:40:04 +0100

    Extract state out of 'useImageProcessing' function

--- frontend/src/composables/image-processing.ts
+++ frontend/src/composables/image-processing.ts
@@ -1,59 +1,59 @@
-import { ref } from "vue";
+import { ref, readonly } from "vue";
 import { main } from "../../wailsjs/go/models";
 import { OpenImageFileSelector, ProccessImage } from "../../wailsjs/go/main/App";
 
-export function useImageProcessing() {
-  const isLoading = ref<boolean>(false);
-  const processedImage = ref<main.ProcessedImage | undefined>();
+const isLoading = ref<boolean>(false);
+const processedImage = ref<main.ProcessedImage | undefined>();
 
-  const setIsLoading = (value: boolean) => {
-    isLoading.value = value;
-  };
+const setIsLoading = (value: boolean) => {
+  isLoading.value = value;
+};
 
-  const setProcessedImage = (image?: main.ProcessedImage) => {
-    processedImage.value = image;
-  };
+const setProcessedImage = (image?: main.ProcessedImage) => {
+  processedImage.value = image;
+};
 
-  const openImageFileSelector = async () => {
-    setIsLoading(true);
+const openImageFileSelector = async () => {
+  setIsLoading(true);
 
-    try {
-      const isFileSelected = await OpenImageFileSelector();
-      if (!isFileSelected) {
-        setIsLoading(false);
-        return;
-      }
-
-      const result = await ProccessImage();
-      setProcessedImage(result);
-    } catch (err) {
-      throw err;
-    } finally {
+  try {
+    const isFileSelected = await OpenImageFileSelector();
+    if (!isFileSelected) {
       setIsLoading(false);
+      return;
     }
-  };
 
-  const resetAppState = () => {
+    const result = await ProccessImage();
+    setProcessedImage(result);
+  } catch (err) {
+    throw err;
+  } finally {
     setIsLoading(false);
-    setProcessedImage(undefined);
-  };
+  }
+};
 
-  const processImage = async () => {
-    setIsLoading(true);
+const resetAppState = () => {
+  setIsLoading(false);
+  setProcessedImage(undefined);
+};
 
-    try {
-      const result = await ProccessImage();
-      setProcessedImage(result);
-    } catch (err) {
-      throw err;
-    } finally {
-      setIsLoading(false);
-    }
-  };
+const processImage = async () => {
+  setIsLoading(true);
 
+  try {
+    const result = await ProccessImage();
+    setProcessedImage(result);
+  } catch (err) {
+    throw err;
+  } finally {
+    setIsLoading(false);
+  }
+};
+
+export function useImageProcessing() {
   return {
-    isLoading,
-    processedImage,
+    isLoading: readonly(isLoading),
+    processedImage: readonly(processedImage),
     openImageFileSelector,
     processImage,
     resetAppState,
