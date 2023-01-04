6c24a60edc016f92c5041ad53fdd3ede64fc6325
Marko Roje <marko.roje@gmail.com>
2023-01-04 19:53:11 +0100

    Implement image export

--- frontend/src/composables/file-manager.ts
+++ frontend/src/composables/file-manager.ts
@@ -6,8 +6,16 @@ const saveProject = () => {
   console.log("save project");
 };
 
-const exportPng = () => {
-  console.log("export png");
+const exportPng = async (imageBase64: string) => {
+  const link = document.createElement("a");
+
+  link.style.display = "none";
+  link.href = imageBase64;
+  link.download = "image-processing-result.png";
+
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 };
 
 export function useFileManager() {
